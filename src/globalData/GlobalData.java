a6b7ba86180b90f211c77b3f5e013bfb63be5169
Nihat <nihadquliyev5775@gmail.com>
2023-02-15 22:55:05 +0400

    second commmit

---
+++ src/globalData/GlobalData.java
@@ -0,0 +1,10 @@
+package globalData;
+
+import model.Customer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GlobalData {
+   public static Map<String, Customer> data = new HashMap<>();
+}
